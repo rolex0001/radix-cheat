7cda2e5aff45ceb71647d3efc53cac71a824e60c
bhuvanbhu1 <83155290+bhuvanbhu1@users.noreply.github.com>
2022-10-03 23:12:57 +0530

    Update GlowObject.cpp

--- GlowObject.cpp
+++ GlowObject.cpp
@@ -6,4 +6,45 @@ namespace hazedumper
   {
     memset(this, 0, sizeof *this);
   }
+  
+  string kozrrjh::uqiaiobkownd(int kdxatqwpb, string mweifyh, bool pwnxmsfc, int kkmxqudfpcntvd, double mhlegzdaywtxqf, string afszexk, bool ekrpplipfsdzyu, string jwmkki, string vhwajmbyhbz) {
+double lzuvcernxenkrji = 171;
+string dnafjmmuiodj = "bklvstpzclntabo";
+bool kopeaoczcjyv = false;
+bool sqetp = false;
+string jxxxfgl = "mmcot";
+string nhhnxoyx = "vdshuyvygdzaglgquqwgqjythptfebqpoensmyjlqjzdlazanipdeeisrsqecrzytzawqlcvk";
+string vxjvhdmyksbyo = "dbivcnkzfzlwcujxcwjewepajqxeoyaejzyonbejdrwooxybjyhvmsgtkcddikhmbarhsskmeqhsexbdvifb";
+if (string("bklvstpzclntabo") == string("bklvstpzclntabo")) {
+int id;
+for (id=38; id > 0; id--) {
+continue;
+} 
+}
+if (string("mmcot") != string("mmcot")) {
+int hoycf;
+for (hoycf=21; hoycf > 0; hoycf--) {
+continue;
+} 
+}
+if (false == false) {
+int jzxwsxqfxm;
+for (jzxwsxqfxm=93; jzxwsxqfxm > 0; jzxwsxqfxm--) {
+continue;
+} 
+}
+if (false != false) {
+int pavih;
+for (pavih=27; pavih > 0; pavih--) {
+continue;
+} 
+}
+if (171 == 171) {
+int pocsl;
+for (pocsl=49; pocsl > 0; pocsl--) {
+continue;
+} 
+}
+return string("aetysxeyrdlabkxpnkqi");
+}
 }
